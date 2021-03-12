0db5e3f2e830d872ab4b7500261b6e16313117ed
Dave Wakeman <dwakeman@us.ibm.com>
2021-03-12 12:53:38 -0600

    adds some comments

--- client/src/util/apiController.js
+++ client/src/util/apiController.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
+/*
+    This controller is running in the client-side React app.  That app is served up from a node.js application that also 
+    serves up an api endpoint that acts as a proxy to the backend API application.  Therefore, the URL we use here to get
+    to the API has the same hostname as this application.  
+
+    However, if it is running locally, the React app (/client/npm start) runs on port 3000, the node app runs on port 3100
+    and the backend app (not in this project) runs on port 3001.  In that scenario we have to manually set the apiUrl to
+    the right value here.
+
+*/
+
 const apiUrl = process.env.NODE_ENV === 'development' ?  'http://localhost:3100' : `https://${window.location.hostname}`;
 console.log('[apiController.js] apiUrl is ' + apiUrl, window.location);
 
 const apiController = axios.create({
-  //baseURL: 'https://resource-controller.cloud.ibm.com/v2'
   baseURL: apiUrl
 });
 
