d4d81659200dc7f0f9e5a47dccc9c54ce75b7a8a
Dave Wakeman <dwakeman@us.ibm.com>
2021-03-12 16:59:53 -0600

    updates ResoureceIntances page with actual data

--- client/src/components/ResourceInstances/ResourceInstances.jsx
+++ client/src/components/ResourceInstances/ResourceInstances.jsx
@@ -6,6 +6,7 @@ import {
   StructuredListHead,
   StructuredListBody,
   StructuredListInput,
+  Loading,
   Icon
 } from "carbon-components-react";
 import { iconCheckmarkSolid } from "carbon-icons";
@@ -13,14 +14,21 @@ import "../../pattern-components/patterns.scss";
 import isDev from '../../util/isDev.ts';
 //import IBMResources from '../../data/ibmResources.json';
 import apiController from '../../util/apiController';
+import regions from '../../util/regions';
+import resourceGroups from '../../util/resourceGroups';
+
 
 class ResourceInstances extends Component {
+
+
+  
 
   constructor(props) {
     super(props);
     this.state = {
       selectedRow: 0,
-      ibmResources: {},
+      ibmResources: null,
+      groups: {},
       isDev: isDev()
     }
 
@@ -29,8 +37,19 @@ class ResourceInstances extends Component {
   componentDidMount () {
     console.log('[ResourceInstances.jsx] componentDidMount()...')
     this.getResources();
+
+    const rGroups = {};
     
+    resourceGroups.forEach(item => {
+      rGroups[item.id] = item.name;
+    });
+    this.setState({groups: rGroups});
+    console.log('the resource groups', this.state.rGroups);    
   }
+
+  // addGroup(group) {
+  //   this.groups[group.id] = group.name;
+  // }
 
   getResources = () => {
     console.log('[ResourceInstances.jsx] getResources');
@@ -56,6 +75,7 @@ class ResourceInstances extends Component {
   };
 
   renderRow = (row, id) => {
+
     return (
       <StructuredListRow key={id} onClick={() => this.onRowClick(id)}>
         <div>
@@ -76,7 +96,16 @@ class ResourceInstances extends Component {
         </div>
 
         <StructuredListCell className="simple-list-row">
-          {row}
+          {row.name}
+        </StructuredListCell>
+        <StructuredListCell className="simple-list-row">
+          {regions[row.region_id] || row.region_id}
+        </StructuredListCell>
+        <StructuredListCell className="simple-list-row">
+          {this.state.groups[row.resource_group_id]}
+        </StructuredListCell>
+        <StructuredListCell className="simple-list-row">
+          {row.state}
         </StructuredListCell>
       </StructuredListRow>
     );
@@ -86,8 +115,17 @@ class ResourceInstances extends Component {
   render ()  {
 
 
-  
-    const data = ["row1", "row2", "row3"];
+
+    let rows = <Loading />;
+    if (this.state.ibmResources) {
+        rows = this.state.ibmResources.resources.map((row, i) => {
+          return this.renderRow(row, i);
+        }
+      )
+    }
+
+  console.log('[ResourceInstances.jsx] starting render() with resources', this.state.ibmResources);
+//    const data = ["row1", "row2", "row3"];
     return (
       <div className="bx--grid pattern-container">
         <h1 className="pattern-title">Resource Instances</h1>
@@ -98,15 +136,22 @@ class ResourceInstances extends Component {
                 <StructuredListRow head>
                   <StructuredListCell head />
                   <StructuredListCell head>
-                    Simple List Title
+                    Resource Name
+                  </StructuredListCell>
+                  <StructuredListCell head>
+                    Region
+                  </StructuredListCell>
+                  <StructuredListCell head>
+                    Resource Group
+                  </StructuredListCell>
+                  <StructuredListCell head>
+                    State
                   </StructuredListCell>
                 </StructuredListRow>
               </StructuredListHead>
 
               <StructuredListBody>
-                {data.map((row, i) => {
-                  return this.renderRow(row, i);
-                })}
+                {rows}
               </StructuredListBody>
             </StructuredListWrapper>
           </div>
---
+++ client/src/util/regions.js
@@ -0,0 +1,12 @@
+const regions = {
+  "global": "Global",
+  "eu-gb": "London",
+  "eu-de": "Frankfurt",
+  "us-south": "Dallas",
+  "us-south-1": "Dallas 1",
+  "us-south-2": "Dallas 2",
+  "us-south-3": "Dallas 3",
+  "us-east": "Washington, DC"
+}
+
+export default regions;
